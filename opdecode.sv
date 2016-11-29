65076f7e35396b7dd4069ecfa7ac7468395dc68c
Mentor Graphics <mg75@ee215lnx14.ecn.purdue.edu>
2016-11-28 21:08:08 -0500

    Added opcode decoder

---
+++ opdecode.sv
@@ -0,0 +1,20 @@
+// $Id: $
+// File name:   OpDecoder.sv
+// Created:     11/22/2016
+// Author:      Ahmad dit Ziad Dannawi
+// Lab Section: 337-03
+// Version:     1.0  Initial Design Entry
+// Description: Opcode decoder
+module opcodeDecoder
+(
+input wire [95:0]opcode,
+output wire [3:0]shape,
+output wire [15:0]color,
+output wire [75:0]opdata
+);
+
+assign shape = opcode[95:92];
+assign color = opcode[91:76];
+assign opdata = opcode[75:0];
+
+endmodule
