ea26e88142d47c25731021b5dee9ae8030c21a64
Ziad <adannawi@purdue.edu>
2016-12-14 16:11:00 -0500

    Fixed resolution in script

--- addressparse.py
+++ addressparse.py
@@ -1,7 +1,7 @@
 import sys
 import numpy as np
 import scipy.misc
-bresen = np.zeros(shape=(640,480))
+bresen = np.zeros(shape=(480,640))
 print(bresen)
 with open('output.txt', 'r') as f:
     read_data = f.readlines()
@@ -11,6 +11,17 @@ y = read_data[1][0:10]
 xInt = int(x, base=2)
 yInt = int(y, base=2)
 for address in read_data:
-    bresen.itemset(((int(address[0:10], base=2)-1),(int(address[10:19], base=2)-1)),1)
+	y = int(address[10:19], base=2)-1
+	x = int(address[0:10], base=2)-1
+	if x < 0:
+		x = 0
+	if x > 640:
+		x = 640
+	if y < 0:
+		y = 0
+	if y > 480:
+		y = 480
+	bresen[y][x] = 1
+
 
 scipy.misc.imsave('outfile.bmp', bresen)
